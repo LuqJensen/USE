5131bb291ef374d2ac0ed09ac599369245a212b2
LuqJensen <lucas__jensen@hotmail.com>
2015-04-19 17:02:05 +0200

    Prepare structure for use with 3 layer model

--- src/unifiedshoppingexperience/UnifiedShoppingExperience.java
+++ src/unifiedshoppingexperience/UnifiedShoppingExperience.java
@@ -2,17 +2,12 @@ package unifiedshoppingexperience;
 
 import java.util.ArrayList;
 import java.util.Map;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 /**
  *
  * @author Gruppe12
  */
-public class UnifiedShoppingExperience extends Application
+public class UnifiedShoppingExperience
 {
     private String email;
     private String phoneNumber;
@@ -33,24 +28,4 @@ public class UnifiedShoppingExperience extends Application
     {
         return null;
     }
-
-    @Override
-    public void start(Stage stage) throws Exception
-    {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-
-        Scene scene = new Scene(root);
-
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args)
-    {
-        launch(args);
-    }
-
 }
