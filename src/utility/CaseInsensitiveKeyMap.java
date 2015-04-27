b68648c459560ca628ae0be1c18e606f2d82968a
LuqJensen <lucas__jensen@hotmail.com>
2015-04-27 16:35:58 +0200

    Kill typesafety and love to fix Map.get

--- src/utility/CaseInsensitiveKeyMap.java
+++ src/utility/CaseInsensitiveKeyMap.java
@@ -20,8 +20,11 @@ public class CaseInsensitiveKeyMap<T> extends HashMap<String, T>
         return super.put(key.toLowerCase(), value);
     }
 
-    public T get(String key)
+    // note, javas implementation is Map<K, V>.get(Object key)...
+    // quite odd having a method break its own class' typesafety...
+    @Override
+    public T get(Object key)
     {
-        return super.get(key.toLowerCase());
+        return super.get(key.toString().toLowerCase());
     }
 }
